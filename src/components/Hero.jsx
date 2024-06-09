76c3932a983cc17a6d5fe793f03c34284cbeb894
Prianshu-git <109914438+Prianshu-git@users.noreply.github.com>
2024-06-09 12:59:50 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -52,12 +52,10 @@ const Hero = () => {
                 <img
                   src={robot}
                   className="w-full scale-[1.7] translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]"
-                  width={0}
-                  height={0}
+                  width={1024}
+                  height={490}
                   alt="AI"
                 />
-
-                <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2" />
 
                 <ScrollParallax isAbsolutelyPositioned>
                   <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
@@ -84,7 +82,7 @@ const Hero = () => {
             <img
               src={heroBackground}
               className="w-full"
-              width={1800}
+              width={1400}
               height={1800}
               alt="hero"
             />
